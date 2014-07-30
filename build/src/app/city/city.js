37d094f2cfe430cf75f19eb8407cb2b12a875232
Andrea Corna <andrea.corna.ac.91@gmail.com>
2014-07-30 18:53:45 +0200

    heroku push
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

---
+++ build/src/app/city/city.js
@@ -0,0 +1,34 @@
+angular.module( 'trippo.city', [
+  'ui.router',
+  'placeholders',
+  'ui.bootstrap' ,
+  'ngResource'
+
+])
+
+.config(function config( $stateProvider ) {
+  $stateProvider.state( 'city', {
+    url: '/city/:city_name',
+    views: {
+      "main": {
+        controller: 'CityCtrl',
+        templateUrl: 'city/city.tpl.html'
+      }
+    },
+    data:{ pageTitle: 'What is It?' }
+  });
+})
+
+.controller( 'CityCtrl', function CityCtrl( $scope, $log , CityRes) {
+  $scope.city =   CityRes.query();
+  $scope.$log= $log;
+
+
+})
+
+.factory( 'CityRes', function ( $resource )  {
+        return $resource("../../city");
+    })
+;
+
+
