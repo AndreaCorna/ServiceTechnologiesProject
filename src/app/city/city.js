e04e91139b0c3ee8e713ab6bb0a11977fc95204d
Andrea Corna <andrea.corna.ac.91@gmail.com>
2014-07-31 16:13:08 +0200

    factory city culture
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

--- src/app/city/city.js
+++ src/app/city/city.js
@@ -15,20 +15,38 @@ angular.module( 'trippo.city', [
         templateUrl: 'city/city.tpl.html'
       }
     },
-    data:{ pageTitle: 'What is It?' }
+    data:{ pageTitle: 'City' }
   });
 })
 
-.controller( 'CityCtrl', function CityCtrl( $scope, $log , CityRes) {
+.controller( 'CityCtrl', function CityCtrl( $scope, $log , CityRes, CultureRes) {
   $scope.city =   CityRes.query();
   $scope.$log= $log;
-
+  $scope.cultureList = CultureRes.list.query();
 
 })
 
 .factory( 'CityRes', function ( $resource )  {
         return $resource("../../city");
     })
-;
 
+.factory( 'CultureRes', function ($resource) {
+        var cultureList = function ($resource) {
+            return $resource("../../city/:city_name/culture", {city_name: "@city_name"});
+
+        };
+
+        var cultureDetails = function($resource,id_culture){
+            return $resource("../../city/:name_city/culture/:id_culture", {name_city: "@city_name",id_culture:id_culture});
+        };
+
+        return {
+            list:function (){
+                cultureList($resource);},
+            details:function (id_culture){
+            cultureDetails($resource,id_culture);}
+        };
+        })
+
+;
 
