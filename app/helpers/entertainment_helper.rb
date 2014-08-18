5ad04e3d5be538755a2114d93258d3a54092aefd
Andrea Corna <andrea.corna.ac.91@gmail.com>
2014-08-18 18:03:32 +0200

    add food resources
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

--- app/helpers/entertainment_helper.rb
+++ app/helpers/entertainment_helper.rb
@@ -1,35 +1,3 @@
 module EntertainmentHelper
 
-  def get_entertainment_items(city)
-    puts 'parameter '+city
-    entertainment_items = []
-    entertainment_items.append(EntertainmentItem.new('lat','long','prova_helper_entertainment',3,3,'photo','icon','reference'))
-    return entertainment_items
-
-  end
-
-  def get_entertainment_details(id)
-    details = []
-    details.append('details entertainment id '+id)
-    return details
-  end
-
-
-  class EntertainmentItem
-    attr_accessor :id,:lat,:lng,:price,:rating,:name,:photo,:icon,:reference;
-
-    def initialize(lat,lng,name,rating,price,photo,icon,reference)
-      @id = name+'_'+lat+'_'+lng;
-      @lat = lat;
-      @lng = lng;
-      @name = name;
-      @rating = rating;
-      @price = price;
-      @photo = photo;
-      @icon = icon;
-      @reference = reference;
-    end
-
-  end
-
 end
