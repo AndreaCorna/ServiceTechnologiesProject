bd58cd245d98051a9748f883e565e365c9d97b1d
Andrea Corna <andrea.corna.ac.91@gmail.com>
2014-08-31 08:40:52 +0200

    change api wikipedia
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

--- app/helpers/freebase_helper.rb
+++ app/helpers/freebase_helper.rb
@@ -1,3 +1,4 @@
+require 'wikipedia'
 module FreebaseHelper
 
   def get_description(name)
@@ -7,6 +8,10 @@ module FreebaseHelper
     if(!best_match.nil?)
       best_match.sync
       description = best_match.description
+      if(description.nil?)
+          page = Wikipedia.find('Ruby on rails')
+          puts page.raw_data
+      end
     end
     return description
   end
