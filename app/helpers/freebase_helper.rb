555f5cc5b85ee02c3d02cc84236c49486845deef
Andrea Corna <andrea.corna.ac.91@gmail.com>
2014-10-01 10:34:07 +0200

    add thread in order to load description of places
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

--- app/helpers/freebase_helper.rb
+++ app/helpers/freebase_helper.rb
@@ -1,3 +1,4 @@
+require 'timeout'
 module FreebaseHelper
   include WikipediaHelper
 
@@ -6,18 +7,31 @@ The method returns the description of a place using freebase api. If the result 
 search is null, it looks for information using the wikipedia api.
 =end
   def get_description(name,city)
-    FreebaseAPI.session = FreebaseAPI::Session.new(key: ENV['API_KEY'], env: :stable)
-
-    results = FreebaseAPI::Topic.search(name+' in '+city)
-    best_match = results.values.first
-    if(!best_match.nil?)
-      best_match.sync
-      description = best_match.description
-      if(description.nil?)
-          description = get_wikipedia_description(name,city)
-      end
+    threads = []
+    descr_freebase = nil
+    descr_wikipedia = nil
+    threads << Thread.new{
+      status = Timeout::timeout(30) {
+        FreebaseAPI.session = FreebaseAPI::Session.new(key: ENV['API_KEY'], env: :stable)
+        results = FreebaseAPI::Topic.search(name+' in '+city)
+        best_match = results.values.first
+        if(!best_match.nil?)
+          best_match.sync
+          descr_freebase = best_match.description
+        end
+      }
+    }
+    threads << Thread.new{
+      descr_wikipedia = get_wikipedia_description(name,city)
+    }
+    threads.each do |thread|
+      thread.join
     end
-    return description
+    if(descr_freebase.nil?)
+      return descr_wikipedia
+    else
+      return descr_freebase
+    end
   end
 
 end
