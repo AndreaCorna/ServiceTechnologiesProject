49c8508cf86ef4c78f373905f87aa6dcfbbaf0d7
Fons <lord.fontana@gmail.com>
2014-10-06 15:54:03 +0200

    add fileupload

---
+++ src/common/file-upload/file-upload.js
@@ -0,0 +1,145 @@
+/**
+ * Created by Lorenzo on 06/10/14.
+ */
+
+/*
+ * jQuery File Upload Plugin Angular JS Example 1.2.1
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2013, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
+ */
+
+/* jshint nomen:false */
+/* global window, angular */
+
+(function () {
+    'use strict';
+
+    var isOnGitHub = true,
+        url = 'https://trippo.s3.amazonaws.com/';
+
+    angular.module('demo', [
+        'blueimp.fileupload'
+    ])
+        .config([
+            '$httpProvider', 'fileUploadProvider',
+            function ($httpProvider, fileUploadProvider) {
+                delete $httpProvider.defaults.headers.common['X-Requested-With'];
+                fileUploadProvider.defaults.redirect = window.location.href.replace(
+                    /\/[^\/]*$/,
+                    '/cors/result.html?%s'
+                );
+
+                    // Demo settings:
+                    angular.extend(fileUploadProvider.defaults, {
+                        // Enable image resizing, except for Android and Opera,
+                        // which actually support image resizing, but fail to
+                        // send Blob objects via XHR requests:
+                        disableImageResize: /Android(?!.*Chrome)|Opera/
+                            .test(window.navigator.userAgent),
+                        maxFileSize: 5000000,
+                        acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
+                    });
+                fileUploadProvider.defaults.handleResponse = function (e,data){
+                    console.log("response data");
+                    console.log(data.result);
+                    console.log("files");
+                    console.log(data.result.files);
+                    var files = data.result && data.result.files;
+                    if (files) {
+                        data.scope.replace(data.files, files);
+                    } else if (data.errorThrown ||
+                        data.textStatus === 'error') {
+                        data.files[0].error = data.errorThrown ||
+                            data.textStatus;
+                    }
+
+                     /*
+                    var files = data.result && data.result.files;
+                    if (files) {
+                        data.scope().replace(data.files, files);
+                        // do what you want...
+                    } else if (data.errorThrown || data.textStatus === 'error') {
+                        data.files[0].error = data.errorThrown ||
+                            data.textStatus;
+                    }
+                    */
+                };
+
+
+            }
+        ])
+
+        .controller('DemoFileUploadController', [
+            '$scope', '$http', '$filter', '$window',
+            function ($scope, $http) {
+                $http({method: 'GET', url: '../guides/s3'}).
+                    success(function (result, status, headers, config) {
+
+
+
+
+                        $('#fileupload').get(0).setAttribute('action', result.url);
+
+                        console.log(result.url);
+
+                            $scope.options = {
+                                //  fileInput:  file.fileInput,
+
+                                type: 'POST',
+                                formData: {AWSAccessKeyId: result['AWSAccessKeyId'],
+                                    key : result['key'],
+                                    policy: result['policy'],
+                                    signature: result['signature'],
+                                    success_action_status: result['success_action_status'],
+                                    acl: result['acl']
+                                },
+                                paramName: 'file', // S3 does not like nested name fields i.e. name="user[avatar_url]"
+                                dataType: 'XML'  // S3 returns XML if success_action_status is set to 201
+
+                            };
+
+
+
+                    });
+
+            }
+        ])
+
+        .controller('FileDestroyController', [
+            '$scope', '$http',
+            function ($scope, $http) {
+                var file = $scope.file,
+                    state;
+                if (file.url) {
+                    file.$state = function () {
+                        return state;
+                    };
+                    file.$destroy = function () {
+                        state = 'pending';
+                        return $http({
+                            url: file.deleteUrl,
+                            method: file.deleteType
+                        }).then(
+                            function () {
+                                state = 'resolved';
+                                $scope.clear(file);
+                            },
+                            function () {
+                                state = 'rejected';
+                            }
+                        );
+                    };
+                } else if (!file.$cancel && !file._index) {
+                    file.$cancel = function () {
+                        $scope.clear(file);
+                    };
+                }
+            }
+        ]);
+
+}());
