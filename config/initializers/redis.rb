5ae55fb120a088c3dcb3c231afff3e6e9a9b3ec5
Fons <lord.fontana@gmail.com>
2014-11-06 23:55:40 +0100

    added secret

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,7 @@
 #This is to use the local redis server
-$redis = Redis.new(host:'localhost', port: 6379)
+#$redis = Redis.new(host:'localhost', port: 6379)
 
 #This is to use the remote redis server
 #$redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+$redis = Redis.new(:url => 'tripporedis.dzrkcw.0001.usw2.cache.amazonaws.com')
+
