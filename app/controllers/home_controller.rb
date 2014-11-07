ca5608828e9ad639fe91c573689bd73388a94d9b
Fons <lord.fontana@gmail.com>
2014-11-07 13:36:36 +0100

    edited env

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    puts '---------------------------------------'
-    puts 'SECRET_KEY_BASE_DEVELOPMENT'
-    puts ENV["SECRET_KEY_BASE_DEVELOPMENT"]
     redirect_to "/UI/#"
   end
 
