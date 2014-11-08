35e5b543f9c0088da4a756f8eb417aa76b7745ca
Fons <lord.fontana@gmail.com>
2014-11-08 12:27:31 +0100

    modified deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,15 +37,7 @@ set :scm, :git
 
 namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
-     on roles(:app), in: :sequence, wait: 5 do
-        # Your restart mechanism here, for example:
 
-         execute "reload_app"
-
-         end
-  end
 
 
 
@@ -60,7 +52,7 @@ namespace :deploy do
     end
   end
   after :publishing, :compile_resources
-  after  :compile_resources , :restart
+
 
 
 
