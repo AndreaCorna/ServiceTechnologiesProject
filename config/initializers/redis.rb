ead28b767d8e9087139ba18d319ac2cc2f6f4d5b
Andrea Corna <andrea.corna.ac.91@gmail.com>
2014-11-16 20:43:03 +0100

    correct redis
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,4 +3,4 @@
 #$redis = Redis.new(host:'localhost', port: 6379)
 
 #This is to use the remote redis server
-redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+$redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
