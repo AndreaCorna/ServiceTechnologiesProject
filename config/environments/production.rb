18e41876d6fdd3af6bcc928113fe8a03b323ddaa
Andrea Corna <andrea.corna.ac.91@gmail.com>
2014-11-21 08:30:49 +0100

    correct host for login
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
 
   #added to solve problem in heroku
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'trippo.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host => 'trippo.cf'}
 
   #added in order to use prerender
   config.middleware.use Rack::Prerender, prerender_token: 'blr6gLxmvkUbz8ZZl8A9'
